ad3296446e2df9c2ad139885780d7fd9f51a2a16
Yeahboi12356 <89899637+Yeahboi12356@users.noreply.github.com>
2021-10-26 15:47:34 +0800

    Update php64.bat

--- php64.bat
+++ php64.bat
@@ -1,4 +1,5 @@
 @echo off
 echo installing php
-powershell -Command "Invoke-WebRequest https://windows.php.net/downloads/releases/php-8.0.12-Win32-vs16-x64.zip -Outfile php.zip && Expand-Archive php.zip"
+powershell -Command "Invoke-WebRequest https://windows.php.net/downloads/releases/php-8.0.12-Win32-vs16-x64.zip -Outfile php.zip"
+powershell -Command "Expand-Archive php.zip"
 echo done
